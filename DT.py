8d9434b01113dd097f3a3120f09e990e929d5b71
Julie_Wang <julie@Julie-Wangs-iMac.local>
2020-05-29 11:15:52 -0700

    finished

--- DT.py
+++
@@ -1,8 +0,0 @@
-#import readr
-import pandas as pd
-
-urlfile="https://raw.githubusercontent.com/profthyagu/Python-Decision-Tree-Using-ID3/master/PlayTennis.csv"
-
-mydata = pd.read_csv(urlfile, error_bad_lines=False)
-mydata.to_csv("Tennis.csv",index=False)
-print(mydata.head())
